a551c92d6b58170ccf0b3b2c29cb731c3a6e06fd
edilson gomes de lima <edskateicevida@gmail.com>
2023-06-18 01:27:01 -0300

    add soma

---
+++ print01.py
@@ -0,0 +1,14 @@
+x = 10
+y = 20
+nome = 'Alberson wander'
+z = x =y
+k = 21.23456789
+restonome = 'Sa dos santos'
+nomecompleto = nome + restonome
+
+print('soma entre {} e {} = {}'.format(x, y, x+y))
+soma=x+y
+
+print('soma entre {} e {} = {}'.format(x, y, soma))
+
+print(f'soma entre {x} e {y} = {soma}')
