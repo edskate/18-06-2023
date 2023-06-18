446fd57cf3d190e62d06933282179fb30af88fd5
edilson gomes de lima <edskateicevida@gmail.com>
2023-06-18 01:12:50 -0300

    add programa python

---
+++ modulos_exemplos_emoji.py
@@ -0,0 +1,8 @@
+x = 10
+y = 20
+nome = 'Alberto wander'
+z = x+y
+k = 21.234789
+restonome = 'sa dos santos'
+nomecompleto = nome + restonome
+print(f'k = {k:.2f}')
---
+++ print.py
@@ -0,0 +1,8 @@
+x = 10
+y = 20
+nome = 'Alberto wander'
+z = x+y
+k = 21.234789
+restonome = 'sa dos santos'
+nomecompleto = nome + restonome
+print('k = {:.2f}'.format(k))
